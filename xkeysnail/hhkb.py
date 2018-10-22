27364447d056d29dd6c418f75de12b3a028acf2d
ryom1m1 <r.imai.ryu@gmail.com>
2018-10-23 02:21:29 +0900

    update dotfiles

--- xkeysnail/hhkb.py
+++ xkeysnail/hhkb.py
@@ -3,19 +3,18 @@ from xkeysnail.transform import *
 
 
 # [Global modemap] Change modifier keys as in xmodmap
-"""
 define_modmap({
     # Swap CapsLock to Left-Ctrl
-    Key.CAPSLOCK: Key.LEFT_CTRL
+    Key.LEFT_SUPER: Key.LEFT_ALT,
+    Key.RIGHT_SUPER: Key.RIGHT_ALT,
 })
-"""
 
 # [Multipurpose modmap] Give a key two meanings. A normal key when pressed and
 # released, and a modifier key when held down with another key. See Xcape,
 # Carabiner and caps2esc for ideas and concept.
 define_multipurpose_modmap({
 	# Capslock is escape when pressed and released. Control when held down.
-	Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL],
+	Key.LEFT_CTRL: [Key.ESC, Key.LEFT_CTRL],
 })
 
 define_keymap(lambda wm_class: wm_class not in ("Pantheon-terminal"), {
@@ -33,7 +32,6 @@ define_keymap(None, {
     K('C-k'): Key.UP,
     K('C-l'): Key.RIGHT,
 }, "Vim-like cursor")
-
 
 define_keymap(re.compile('Pantheon-terminal'), {
     K('esc'): [K('left_alt'), K('esc')],
