c25e80b13e4fb53ddcd888b7b715b21c38357bf8
Ryo Imai <iryo@jp.ibm.com>
2018-11-03 17:42:41 +0900

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
+# fisherman settings
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
@@ -13,9 +14,14 @@ for file in $fisher_path/conf.d/*.fish
     builtin source $file 2> /dev/null
 end
 
+#shell
+set SHELL (which fish)
+
+# Powerline
 set -g theme_display_k8s_context yes
 set -g theme_display_virtualenv yes
 
+# fzf
 set FZF_LEGACY_KEYBINDINGS 0
 
 #pyenv
@@ -25,4 +31,8 @@ status --is-interactive; and . (pyenv virtualenv-init -|psub)
 
 #neovim
 set XDG_CONFIG_HOME $HOME"/.config"
+balias vi nvim
 
+#kubectl
+balias k 'kubectl'
+balias kube 'kubectl'
