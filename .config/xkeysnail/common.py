3c996836f4828d258cf3691fb3be8b334aa2b09e
ryom1m1 <r.imai.ryu@gmail.com>
2019-02-11 01:48:38 +0900

    update key mappings by xkeysnail
    - merge common settings into common.py
    - Mac-like zenkaku-hankaku conversion
    - Vim-like cursor moving with Super-key

--- .config/xkeysnail/common.py
+++ .config/xkeysnail/common.py
@@ -1,13 +1,8 @@
 import re
 from xkeysnail.transform import *
 
-
-# [Global modemap] Change modifier keys as in xmodmap
 """
-define_modmap({
-    # Swap CapsLock to Left-Ctrl
-    Key.CAPSLOCK: Key.LEFT_CTRL
-})
+Common key maps across any key-boards
 """
 
 # [Multipurpose modmap] Give a key two meanings. A normal key when pressed and
@@ -16,6 +11,9 @@ define_modmap({
 define_multipurpose_modmap({
 	# Capslock is escape when pressed and released. Control when held down.
 	Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL],
+        # Map D to Super when held down
+        # to enable Vim-like cursor
+	# Key.D: [Key.D, Key.LEFT_META]
 })
 
 define_keymap(lambda wm_class: wm_class not in ("Pantheon-terminal"), {
@@ -24,21 +22,25 @@ define_keymap(lambda wm_class: wm_class not in ("Pantheon-terminal"), {
 	K("Alt-v"): K("C-v"),
 	K("Alt-l"): K("C-l"),
 	K("Alt-w"): K("C-w"),
-	K("Alt-q"): K("C-q")
+	K("Alt-q"): K("C-q"),
+        # hankaku, zenkaku
+	K("C-j"): Key.F10,
+	K("C-k"): Key.F7,
+	K("C-l"): Key.F9,
+	K("C-SEMICOLON"): Key.F8
 }, "Mac-like settings")
 
 define_keymap(None, {
-    K('C-h'): Key.LEFT,
-    K('C-j'): Key.DOWN,
-    K('C-k'): Key.UP,
-    K('C-l'): Key.RIGHT,
+    K('Super-h'): Key.LEFT,
+    K('Super-j'): Key.DOWN,
+    K('Super-k'): Key.UP,
+    K('Super-l'): Key.RIGHT
 }, "Vim-like cursor")
-
 
 define_keymap(re.compile('Pantheon-terminal'), {
     K('esc'): [K('left_alt'), K('esc')],
-	K("Alt-c"): K("C-Shift-c"),
-	K("Alt-v"): K("C-Shift-v"),
-	K("Alt-t"): K("C-Shift-t")
+    K("Alt-c"): K("C-Shift-c"),
+    K("Alt-v"): K("C-Shift-v"),
+    K("Alt-t"): K("C-Shift-t")
 }, "Esc and IME off for vim")
 
